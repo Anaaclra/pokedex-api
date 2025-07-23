8716925ffc508d53952e640c3b31cf656f8ce2b5
Ana Clara <anaclaraa02pereira@gmail.com>
2025-07-22 21:38:13 -0300

    Implementação dos Endpoints PUT e DELETE, correção dos Endpoints anteriores e validação através do Postman

--- pokedex-api/src/main/java/com/project/pokedex/controller/PokemonController.java
+++ pokedex-api/src/main/java/com/project/pokedex/controller/PokemonController.java
@@ -1,26 +1,58 @@
 package com.project.pokedex.controller;
 
+import com.project.pokedex.entities.Pokemon;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.project.pokedex.service.PokemonService;
 
 
 @RestController
 @RequestMapping("/v1/pokemon")
 public class PokemonController {
 
-    //Ajustar o post para que seja possível registrar novos pokemons
-    // Exemplo Estrutura JSON: { "numero":1, "nome":"Bulbasauro", "tipos": ["grama","venenoso"]}
+    private final PokemonService pokemonService;
+
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }//Construtor para o metodo private final
+
+    // Endpoint POST
     @PostMapping
-    public String adicionarPokemon() {
-        return "Registra Pokemons";
+    public ResponseEntity<String> adicionarPokemon(@RequestBody Pokemon pokemon) {
+        pokemonService.adicionarPokemon(pokemon);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Pokemon registrado com sucesso.");
+    }
+    // Endpoint GET
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPokemon(@PathVariable Integer id) {
+        Pokemon pokemon = pokemonService.buscarPokemon(id);
+        if (pokemon != null) {
+            return ResponseEntity.ok(pokemon);
+        } else {
+            return ResponseEntity.noContent().build(); // retorna 204 se não encontra o pokemon na base.
+        }
     }
 
-    @GetMapping("/{id")
-    public String buscarPokemon(@PathVariable Integer id) {
-        return "Meu Pokemon" + id;
-        //"204"
+     // Endpoint PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizarPokemon(@PathVariable Integer id, @RequestBody Pokemon novoPokemon) {
+        boolean atualizado = pokemonService.atualizarPokemon(id, novoPokemon);
+        if (atualizado) {
+            return ResponseEntity.ok("Pokemon atualizado com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pokemon não encontrado.");
+        }
     }
-
-    //Incluir mais dois endpoints e realizar seus devidos testes através do postman
-    // Endpoint PUT
     // Endpoint DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarPokemon(@PathVariable Integer id) {
+        boolean removido = pokemonService.deletarPokemon(id);
+        if (removido) {
+            return ResponseEntity.ok("Pokemon removido com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pokemon não encontrado.");
+        }
+    }
 }
+
--- pokedex-api/src/main/java/com/project/pokedex/service/PokemonService.java
+++ pokedex-api/src/main/java/com/project/pokedex/service/PokemonService.java
@@ -2,19 +2,21 @@ package com.project.pokedex.service;
 
 import com.project.pokedex.entities.Pokemon;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class PokemonService {
 
     private final List<Pokemon> pokemons = new ArrayList<>();
 
+    //post
     public void adicionarPokemon(Pokemon pokemon) {
         this.pokemons.add(pokemon); //Criei um objeto pra listar os pokemons
     }
 
+    //get
     //mudar a repetição for para filter exemplos no site baeldung
    public Pokemon buscarPokemon(Integer numero) {
         for (int i = 0; i < pokemons.size(); i++) {
@@ -25,4 +27,20 @@ public class PokemonService {
         }
         return null;
     }
+    //put
+    public boolean atualizarPokemon(Integer numero, Pokemon novoPokemon) {
+        for (int i = 0; i < pokemons.size(); i++) {
+            Pokemon atual = pokemons.get(i);
+            if (atual.getNumero().equals(numero)) {
+                pokemons.set(i, novoPokemon);
+                return true;
+            }
+        }
+        return false;
+    }
+    //delete
+    public boolean deletarPokemon(Integer numero) {
+        return pokemons.removeIf(pokemon -> pokemon.getNumero().equals(numero));
+    }
 }
+
